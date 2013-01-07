3d1822cf78d973a136a3cc79b62d3c08b5b57312
haraacharya <hara.acharya@gmail.com>
2013-01-07 10:44:40 -0800

    Added display_show_page_title method to helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,16 +11,28 @@ module PostsHelper
 				
 	end	
 
+	def display_show_title(post)
+		if post.url.starts_with?("http://")
+			return link_to post.title, post.url
+		elsif post.url.to_s == ''
+			return post.title
+		else
+			return link_to post.title, "http://" + post.url
+		end
+				
+	end
+
+
 	def display_post_comments(post)
 		if post.comments.count <= 0
 			return "Be the first to comment!"
 		else
 			str = ""
-			post.comments.reload.each do |comment|
+			post.comments.reload.order("created_at DESC").each do |comment|
 				str << "<li>" + comment.content + "</li>"
 			end 
 			return str.html_safe
 		end
 	end
-	
+
 end	
