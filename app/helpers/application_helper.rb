24625fa1afb9eabd5178fc36343ff33b0a0e348a
haraacharya <hara.acharya@gmail.com>
2013-01-17 14:59:22 -0800

    implemented active-pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def active(page)
+  		return 'class="active"'.html_safe if current_page?(page)
+  	end
+
 	def display_category
 		@categories = Category.all
 		str = ""
